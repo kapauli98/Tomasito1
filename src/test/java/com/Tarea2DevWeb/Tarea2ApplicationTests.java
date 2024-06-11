f30a0b01d8003b73dbb3f8bc06e04245d02cd11e
kapau <kapau@Kapauli98>
2024-06-11 13:28:10 -0600

    Ejemplos basicos + CSS -Semana02

---
+++ src/test/java/com/Tarea2DevWeb/Tarea2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea2DevWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
